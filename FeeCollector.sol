8190bbe235a36fadae557e2e7aa04624917bd0d3
Remix Workspace <>
2022-09-17 13:53:01 +0300

    FeeCollector-Contract-Completed

---
+++ FeeCollector.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+contract FeeCollector{
+    address public owner;
+    uint256 public balance;
+    constructor(){
+        owner = msg.sender;
+    }   
+    receive() payable external {
+        balance += msg.value;
+    }
+
+    function withdraw(uint amount, address payable destAddr) public {
+        require(msg.sender == owner, "Only owner can withdraw");
+        require(amount <= balance, "Insfuccient funds");
+        destAddr.transfer(amount);
+        balance -= amount;
+    }
+}
